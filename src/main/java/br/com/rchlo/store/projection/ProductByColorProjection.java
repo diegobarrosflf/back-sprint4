1aa7da9d92b65a3b0af1521f736b836e6c868ed6
diego <diegobarrosflf@gmail.com>
2021-06-29 22:48:37 -0300

    @DataJpaTest no ProductRepositoryTest

--- src/main/java/br/com/rchlo/store/projection/ProductByColorProjection.java
+++ src/main/java/br/com/rchlo/store/projection/ProductByColorProjection.java
@@ -1,6 +1,13 @@
 package br.com.rchlo.store.projection;
 
+import br.com.rchlo.store.domain.Color;
+
 public interface ProductByColorProjection {
-    String getColor();
-    String getAmount();
+    Color getColor();
+    int getAmount();
+
+
+    default String getColorDescription() {
+        return getColor().getDescription();
+    }
 }
--- src/test/java/br/com/rchlo/store/repository/ProductRepositoryTest.java
+++ src/test/java/br/com/rchlo/store/repository/ProductRepositoryTest.java
@@ -6,111 +6,80 @@ import br.com.rchlo.store.domain.Category;
 import br.com.rchlo.store.domain.Color;
 import br.com.rchlo.store.domain.Product;
 import br.com.rchlo.store.dto.ProductByColorDto;
+import br.com.rchlo.store.projection.ProductByColorProjection;
 import br.com.rchlo.store.repository.util.JPAUtil;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.util.comparator.Comparators;
 
 import javax.persistence.EntityManager;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 class ProductRepositoryTest {
 
-    private EntityManager entityManager;
+    @Autowired
     private ProductRepository productRepository;
-
-    @BeforeEach
-    public void beforeEach() {
-       /* this.entityManager = JPAUtil.getEntityManager();
-        this.productRepository = new ProductRepository(this.entityManager);
-        this.entityManager.getTransaction().begin();*/
-    }
-
-    @AfterEach
-    public void afterEach() {
-        this.entityManager.getTransaction().rollback();
-    }
 
     @Test
     void shouldListAllProductsOrderedByName() {
-        /*Category infantil = aCategory();
 
-        aProduct(infantil);
-        anotherProduct(infantil);
+        Pageable page = PageRequest.of(0, 10, Sort.by(Sort.Direction.ASC, "name"));
+        List<Product> products = productRepository.findAllWithImagesCategoryAndSizesOrderByName(page);
 
-        List<Product> products = productRepository.findAllWithImagesCategoryAndSizesOrderByName();
+        assertEquals(5, products.size());
 
-        assertEquals(2, products.size());
+        List<Product> productsSortByName = new ArrayList<>(products);
+        productsSortByName.sort(Comparator.comparing(Product::getName));
 
         Product firstProduct = products.get(0);
-        assertEquals(7L, firstProduct.getCode());
-        assertEquals("Jaqueta Puffer Juvenil Com Capuz Super Mario Branco", firstProduct.getName());
+        Product firstProductExpected = productsSortByName.get(0);
+        assertEquals(firstProductExpected.getCode(), firstProduct.getCode());
+        assertEquals(firstProductExpected.getName(), firstProduct.getName());
 
         Product secondProduct = products.get(1);
-        assertEquals(1L, secondProduct.getCode());
-        assertEquals("Regata Infantil Mario Bros Branco", secondProduct.getName());*/
+        Product secondProductExpected = productsSortByName.get(1);
+        assertEquals(secondProductExpected.getCode(), secondProduct.getCode());
+        assertEquals(secondProductExpected.getName(), secondProduct.getName());
     }
 
     @Test
     void shouldRetrieveProductsByColor() {
-       /* Category infantil = aCategory();
 
-        aProduct(infantil);
-        anotherProduct(infantil);
-        yetAnotherProduct(infantil);
+        List<ProductByColorProjection> productsByColor = productRepository.productsByColor();
+        Collections.sort(productsByColor, Comparator.comparing(ProductByColorProjection::getColor));
 
-        List<ProductByColorDto> productsByColor = productRepository.productsByColor();
-        Collections.sort(productsByColor, Comparator.comparing(ProductByColorDto::getColor));
+        assertEquals(3, productsByColor.size());
 
-        assertEquals(2, productsByColor.size());
+        ProductByColorProjection firstProductByColorProjection = productsByColor.get(0);
+        assertEquals(Color.BLUE.getDescription(), firstProductByColorProjection.getColorDescription());
+        assertEquals(1, firstProductByColorProjection.getAmount());
 
-        ProductByColorDto firstProductDto = productsByColor.get(0);
-        assertEquals(Color.WHITE.getDescription(), firstProductDto.getColor());
-        assertEquals(2, firstProductDto.getAmount());
+        ProductByColorProjection SecondProductByColorProjection = productsByColor.get(1);
+        assertEquals(Color.RED.getDescription(), SecondProductByColorProjection.getColorDescription());
+        assertEquals(1, SecondProductByColorProjection.getAmount());
 
-        ProductByColorDto secondProductDto = productsByColor.get(1);
-        assertEquals(Color.RED.getDescription(), secondProductDto.getColor());
-        assertEquals(1, secondProductDto.getAmount());*/
+        ProductByColorProjection thirdProductByColorProjection = productsByColor.get(2);
+        assertEquals(Color.WHITE.getDescription(), thirdProductByColorProjection.getColorDescription());
+        assertEquals(3, thirdProductByColorProjection.getAmount());
 
     }
 
-    private Category aCategory() {
-        Category infantil = new CategoryBuilder().withName("Infantil").withSlug("infantil").withPosition(1).build();
-        entityManager.persist(infantil);
-        return infantil;
-    }
-
-    private void aProduct(Category category) {
-        Color color = Color.WHITE;
-        entityManager.persist(new ProductBuilder().withCode(7L)
-                .withName("Jaqueta Puffer Juvenil Com Capuz Super Mario Branco")
-                .withDescription("A Jaqueta Puffer Juvenil Com Capuz Super Mario Branco é confeccionada em material sintético.")
-                .withSlug("jaqueta-puffer-juvenil-com-capuz-super-mario-branco-13834193_sku").withBrand("Nintendo")
-                .withPrice(new BigDecimal("199.90")).withDiscount(null).withColor(color).withWeightInGrams(147)
-                .withCategory(category).build());
-    }
-
-    private void anotherProduct(Category category) {
-        Color color = Color.WHITE;
-        entityManager.persist(new ProductBuilder().withCode(1L).withName("Regata Infantil Mario Bros Branco")
-                .withDescription("A Regata Infantil Mario Bros Branco é confeccionada em fibra natural. Aposte!")
-                .withSlug("regata-infantil-mario-bros-branco-14040174_sku").withBrand("Nintendo")
-                .withPrice(new BigDecimal("29.90")).withDiscount(null).withColor(color).withWeightInGrams(98)
-                .withCategory(category).build());
-    }
-
-    private void yetAnotherProduct(Category infantil) {
-        Color color = Color.RED;
-        entityManager.persist(new ProductBuilder().withCode(2L).withName("Blusa de Moletom Infantil Mario Bros Vermelho")
-                .withDescription("A Blusa de Moletom Infantil Mario Bros Vermelho é quentinha e divertida!")
-                .withSlug("blusa-infantil-moletom-mario-bros-vermelho-14125129_sku").withBrand("Nintendo")
-                .withPrice(new BigDecimal("79.90")).withDiscount(null).withColor(color).withWeightInGrams(126)
-                .withCategory(infantil).build());
-    }
 }
